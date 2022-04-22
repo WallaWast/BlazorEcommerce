0f6f29624fa6cb6b65565c1aefd5ae4c748c718a
Wallace Torres <wallacewst@gmail.com>
2022-04-22 15:38:30 -0700

    Product detail

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,12 @@
 
         public List<Product> Products { get; set; } = new();
 
+        public async Task<ServiceResponse<Product>> GetProductById(int productId)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
