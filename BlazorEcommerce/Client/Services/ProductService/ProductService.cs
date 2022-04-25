b4b0c54f07273447829c9919d60a05854a844b1c
Wallace Torres <wallacewst@gmail.com>
2022-04-25 10:46:10 -0700

    Returning products by category

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -11,18 +11,24 @@
 
         public List<Product> Products { get; set; } = new();
 
+        public event Action ProductsChanged;
+
         public async Task<ServiceResponse<Product>> GetProductById(int productId)
         {
             var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/{productId}");
             return result;
         }
 
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
+            var result = categoryUrl == null ?
+                    await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product") :
+                    await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/Product/category/{categoryUrl}");
 
             if (result != null && result.Data != null)
                 Products = result.Data;
+
+            ProductsChanged.Invoke();
         }
     }
 }
