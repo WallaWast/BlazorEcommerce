c7dbf1a245485f70b580c8b981006fc32c0fdfa1
Wallace Torres <wallacewst@gmail.com>
2022-04-25 09:20:23 -0700

    Get Category on the menu

---
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly HttpClient _http;
+
+        public CategoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task GetCategories()
+        {
+            var respose = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
+
+            if (respose != null && respose.Data != null)
+                Categories = respose.Data;
+        }
+    }
+}
